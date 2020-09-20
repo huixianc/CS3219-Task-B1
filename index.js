0a411a7bfc09d7cb2aa608d0eefb1379f44f9568
huixianc <50432304+huixianc@users.noreply.github.com>
2020-09-20 15:41:10 +0800

    resolve deprecation warning

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/resthub', { useUnifiedTopology: true, useNewUrlParser: true });
 var db = mongoose.connection;
 
 // Added check for DB connection
