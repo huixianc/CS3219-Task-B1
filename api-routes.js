1388ad1a2654ca79eeea0520a959bdbc920f337e
huixianc <50432304+huixianc@users.noreply.github.com>
2020-09-20 13:51:59 +0800

    followed tutorial + some edits

---
+++ api-routes.js
@@ -0,0 +1,34 @@
+// Initialize express router
+let router = require('express').Router();
+
+// Import contact controller
+var contactController = require('./contactController');
+
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API It is Working',
+        message: 'Welcome to RESTHub crafted with love!'
+    });
+});
+
+
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index);
+    
+router.route('/contacts/add')
+    .post(contactController.new);
+    
+router.route('/contacts/:contact_id')
+    .get(contactController.view);
+
+router.route('/contacts/update/:contact_id')
+    .patch(contactController.update)
+    .put(contactController.update);
+
+router.route('/contacts/delete/:contact_id')
+    .delete(contactController.delete);
+
+// Export API routes
+module.exports = router;
--- index.js
+++ index.js
@@ -1,84 +1,39 @@
+// import Express and create app
 const express = require('express');
 const app = express();
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
 
-app.use(express.json());
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-const courses = [
-    {id: 1, name:'course 1'},
-    { id: 2, name: 'course 2' },
-    { id: 3, name: 'course 3' },
-    { id: 4, name: 'course 4' },
+// Connect to Mongoose and set connection variable
+mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true });
+var db = mongoose.connection;
 
-]
+// Added check for DB connection
+if (!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
 
+
+// send message for default url
 app.get('/', (req, res) => {
     res.send('Hello World! :)');
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
 
-app.post('/api/courses', (req, res) => {
-    // check course name validity
-    if (!req.body.name) {
-        res.status(404).send('Name is required!');
-        return;
-    }
-    
-    let course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-
-    courses.push(course);
-    res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-    // look up course 
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found');
-        return;
-    }
-    // check course name validity
-    if (!req.body.name) {
-        res.status(404).send('Name is required!');
-        return;
-    }
-
-    course.name = req.body.name;
-    res.send(course);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    // look up course 
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found');
-        return;
-    }
-
-    res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    // look up course 
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found');
-        return;
-    }
-
-    // delete course
-    let index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-
-});
+// Import routes
+let apiRoutes = require("./api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
 
 const port = process.env.PORT || 3000;
-
+// launch app to listen to port
 app.listen(port, () => console.log(`Listening on port ${port}`));  
